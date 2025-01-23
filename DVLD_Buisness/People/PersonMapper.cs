925e614ac37758c78f33a897a746790d2c169325
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-01-23 09:51:14 +0100

    Refactor: Add 'PersonMapper' and 'PersonValidator'
    - Created 'PersonValidator' class to handle validation logic.
    - Created 'PersonMapper' class to map data between  and .
    - Integrated these classes into .

---
+++ DVLD_Buisness/People/PersonMapper.cs
@@ -0,0 +1,53 @@
+﻿using DVLD.DTOs;
+
+namespace DVLD_Business
+{
+    public class PersonMapper
+    {
+        public PersonDTO ToDTO(Person person)
+        {
+            return new PersonDTO
+            {
+                PersonID = person.PersonID,
+                FirstName = person.FirstName,
+                SecondName = person.SecondName,
+                ThirdName = person.ThirdName,
+                LastName = person.LastName,
+                NationalNo = person.NationalNo,
+                DateOfBirth = person.DateOfBirth,
+                Gender = person.Gender,
+                Address = person.Address,
+                Phone = person.Phone,
+                Email = person.Email,
+                NationalityCountryID = person.NationalityCountryID,
+                ImagePath = person.ImagePath
+            };
+        }
+
+        public Person FromDTO(PersonDTO dto)
+        {
+            return new Person
+            {
+                PersonID = dto.PersonID,
+                FirstName = dto.FirstName,
+                SecondName = dto.SecondName,
+                ThirdName = dto.ThirdName,
+                LastName = dto.LastName,
+                NationalNo = dto.NationalNo,
+                DateOfBirth = dto.DateOfBirth,
+                Gender = dto.Gender,
+                Address = dto.Address,
+                Phone = dto.Phone,
+                Email = dto.Email,
+                NationalityCountryID = dto.NationalityCountryID,
+                ImagePath = dto.ImagePath
+            };
+        }
+    }
+
+
+}
+
+
+
+﻿
