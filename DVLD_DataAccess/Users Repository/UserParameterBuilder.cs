544972a09c2a6b7c284f04793e32f94791bd749c
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-01-25 15:07:14 +0100

    Refactor: Add UserParameterBuilder class
    - Added 'UserParameterBuilder' class to encapsulate logic for filling 'SqlCommand' parameters.
    - Adheres to the DRY (Don't Repeat Yourself) principle.

---
+++ DVLD_DataAccess/Users Repository/UserParameterBuilder.cs
@@ -0,0 +1,16 @@
+﻿using DVLD.DTOs;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public static class UserParameterBuilder
+    {
+        public static void FillSqlCommandParameters(SqlCommand command, UsersDTO user)
+        {
+            command.Parameters.AddWithValue("@PersonID", user.PersonID);
+            command.Parameters.AddWithValue("@UserName", user.Username);
+            command.Parameters.AddWithValue("@Password", user.Password);
+            command.Parameters.AddWithValue("@IsActive", user.IsActive);
+        }
+    }
+}
