626cecc547d69c51080bc76e1e4f771974ef0431
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-01-25 14:58:24 +0100

    Refactor: Add UserDataMapper class
    - Added 'UserDataMapper' class to encapsulate mapping logic between database results and .
    - Adheres to the DRY (Don't Repeat Yourself) principle.

---
+++ DVLD_DataAccess/Users Repository/UserDataMapper.cs
@@ -0,0 +1,20 @@
+﻿using DVLD.DTOs;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public static class UserDataMapper
+    {
+        public static UsersDTO MapToUserDTO(SqlDataReader reader)
+        {
+            return new UsersDTO
+            {
+                UserID = (int)reader["UserID"],
+                PersonID = (int)reader["PersonID"],
+                Username = (string)reader["UserName"],
+                Password = (string)reader["Password"],
+                IsActive = (bool)reader["IsActive"]
+            };
+        }
+    }
+}
