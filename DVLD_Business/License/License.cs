26261af86bdd7f19cba261b38b97465af38d81ec
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-01-29 11:06:35 +0100

    refactor: Apply SRP to 'clsLicense' class
    - Split 'clsLicense' into focused service classes: LicenseService, LicenseRepoService, LicenseMapper, and LicenseValidator.
    - Move enIssueReason enum to namespace level.
    - Update method signatures to align with the new service pattern.
    
    Breaking changes:
    
    - Replaced clsLicense with dedicated service classes.

--- DVLD_Business/License/License.cs
+++ DVLD_Business/License/License.cs
@@ -1,23 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DVLD.DTOs
+namespace DVLD_Business
 {
-    public class LicenseDTO
+    public class License
     {
+        public int DriverId { get; set; }
+        public clsDriver Driver { get; set; }
         public int LicenseId { set; get; }
         public int AppId { set; get; }
-        public int DriverId { set; get; }
-        public int LicenseClassId { set; get; }
+        public clsLicenseClass LicenseClass { set; get; }
         public DateTime IssueDate { set; get; }
         public DateTime ExpirationDate { set; get; }
         public string Notes { set; get; }
         public float PaidFees { set; get; }
         public bool IsActive { set; get; }
-        public short IssueReasonId { set; get; }
+        public string IssueReasonText { set; get; }
         public int UserID { set; get; }
+        public int DetainedLicenseId { set; get; }
+        public bool IsDetained { set; get; }
     }
+
+    
 }
---
+++ DVLD_Business/License/LicenseMapper.cs
@@ -0,0 +1,49 @@
+﻿using DVLD.DTOs;
+
+namespace DVLD_Business
+{
+    public class LicenseMapper
+    {
+        public LicenseDTO ToDTO(License license)
+        {
+            return new LicenseDTO()
+            {
+                AppId = license.AppId,
+                LicenseId = license.LicenseId,
+                DriverId = license.Driver.DriverID,
+                LicenseClassId = license.LicenseClass.LicenseClassID,
+                IssueDate = license.IssueDate,
+                ExpirationDate = license.ExpirationDate,
+                Notes = license.Notes,
+                PaidFees = license.PaidFees,
+                IsActive = license.IsActive,
+                IssueReasonId = LicenseHelpers.GetIssueReasonId(license.IssueReasonText),
+                UserID = license.UserID
+            };
+        }
+
+        public License FromDTO(LicenseDTO dto)
+        {
+            return new License()
+            {
+                Driver = clsDriver.FindByDriverID(dto.DriverId),
+                LicenseId = dto.LicenseId,
+                AppId = dto.AppId,
+                LicenseClass = clsLicenseClass.Find(dto.LicenseClassId),
+                IssueDate = dto.IssueDate,
+                ExpirationDate = dto.ExpirationDate,
+                Notes = dto.Notes,
+                PaidFees = dto.PaidFees,
+                IsActive = dto.IsActive,
+                IssueReasonText = LicenseHelpers.GetIssueReasonText(dto.IssueReasonId),
+                DetainedLicenseId = clsDetainedLicense.FindByLicenseID(dto.LicenseClassId).DetainID,
+                UserID = dto.UserID,
+                IsDetained = clsDetainedLicense.IsLicenseDetained(dto.LicenseId)
+            };
+
+        }
+
+    }
+
+    
+}
