67e2f4e351cd9956f8a6c1352748bdca930a94ec
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-02 18:23:18 +0100

    feat: Add EFcore to my project
    - Downloaded EFcore NuGet package.
    - Added new 'AppDbContext' class for database session.
    - Added 'DbSet<Person>' for person modeling.
    - Added a unique index for the 'NationalNo' property in the 'Person' entity.
    - Implemented gender conversion to ensure proper data type handling.

---
+++ DVLD_NetCore/DVLD_DataAccess.Core/Context/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using DVLD_DataAccess.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DVLD_DataAccess.Core.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {   }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.HasIndex(p => p.NationalNo)
+                .IsUnique();
+
+                entity.Property(p => p.NationalNo)
+                .HasMaxLength(20);
+
+                entity.Property(x => x.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+                entity.Property(x => x.SecondName).HasMaxLength(50);
+                entity.Property(x => x.ThirdName).HasMaxLength(50);
+                
+                entity.Property(x => x.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+
+                entity.Property(x => x.Gender)
+                      .HasConversion<int>();
+            });
+        }
+    }
+}
