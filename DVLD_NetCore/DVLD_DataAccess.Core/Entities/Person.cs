4defe3823735e72009e59c5cff59d9bd1b542596
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-02 18:22:38 +0100

    feat: Migrate 'Person' class to .Net Core
    - Migrated 'Person' class to .net core.
    - Added new C# features ('required' and 'nullable') modifiers to properties in the 'Person' class.

---
+++ DVLD_NetCore/DVLD_DataAccess.Core/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using DVLD_DataAccess.Core.Enums;
+
+namespace DVLD_DataAccess.Core.Entities
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public required string FirstName { get; set; }
+        public string? SecondName { get; set; }
+        public string? ThirdName { get; set; }
+        public required string LastName { get; set; }
+        public required string NationalNo { get; set; }
+        public required DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public int CountryId { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
