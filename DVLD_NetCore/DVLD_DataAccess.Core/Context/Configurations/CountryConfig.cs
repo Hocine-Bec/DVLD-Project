2e040b2d2d61a2a0d750d1ff7dd10be28568eef4
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-08 15:24:35 +0100

    feat: Add 'Configurations' folder for EF Core entity configurations
    - Created a new folder named 'Configurations' to hold entity configurations.
    - Added configuration for the 'Person' and 'Country' entities to define its properties and constraints.

---
+++ DVLD_NetCore/DVLD_DataAccess.Core/Context/Configurations/CountryConfig.cs
@@ -0,0 +1,28 @@
+﻿using DVLD_DataAccess.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess.Core.Context.Configurations
+{
+    public class CountryConfig
+    {
+        public void Configure(EntityTypeBuilder<Country> entity)
+        {
+            entity.ToTable("Countries");
+
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Id).IsRequired();
+
+            entity.Property(x => x.CountryName)
+                    .HasMaxLength(50)
+                    .IsRequired()
+                    .HasColumnType("nvarchar(50)");
+        }
+    }
+}
